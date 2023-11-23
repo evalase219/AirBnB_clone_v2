bedac7f853948120ce8fb4158d808f145d484d40
evalase219 <chiomaevangeline85@gmail.com>
2023-11-22 17:22:34 -0800

    MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- Create or use the existing database hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create or use the existing user hbnb_test with the password 'hbnb_test_pwd' restricted to localhost
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges on hbnb_test_db to hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_test
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
