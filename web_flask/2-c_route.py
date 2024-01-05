02c1f39f329cb573fe43dd353d40f7371fdb7e5e
evalase219 <chiomaevangeline85@gmail.com>
2024-01-05 14:55:03 -0800

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" Starts a Flask Web Application HBNB"""
+""" Starts a Flask Web Application C is FUN"""
 from flask import Flask
 app = Flask(__name__)
 
@@ -17,6 +17,14 @@ def hbnb():
     return 'HBNB'
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ Prints a Message when /c is called """
+    return "C " + text.replace('_', ' ')
+
+
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port=5000)
+
+
